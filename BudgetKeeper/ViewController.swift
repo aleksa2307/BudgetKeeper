4bcc493cec3359a31ef020f258f7589765465871
andreichenko256 <andreichenko.kirill2000@gmail.com>
2026-05-16 00:00:21 +0300

    Initial Commit

---
+++ BudgetKeeper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BudgetKeeper
+//
+//  Created by Kyrylo Andreichenko on 16.05.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
